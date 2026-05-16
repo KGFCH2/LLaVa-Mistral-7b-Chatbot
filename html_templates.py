986b3b3b4b74ff789c1a17086cfb18a6ec3fdb85
KGFCH2 <babinbid05@gmail.com>
2026-05-17 05:28:29 +0530

    Fixed

--- html_templates.py
+++ html_templates.py
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-css = """
-    <style>
-        /* User Chat Message */
-
-        .st-emotion-cache-janbn0 {
-            background-color: #2b313e;
-            text-color: black;
-        }
-
-        /* AI Chat Message */
-
-        .st-emotion-cache-4oy321 {
-            background-color: #b3c8f2;
-            text-color: black;
-        }
-
-        section[data-testid="stSidebar"] {
-            width: 380px !important;
-        }
-
-        .footer-container {
-            margin-top: 5rem;
-            padding: 2rem;
-            border-top: 1px solid #4a5568;
-            text-align: center;
-        }
-
-        .divider-line {
-            margin: 2rem 0;
-            border-bottom: 1px solid #4a5568;
-        }
-    </style>
-=======
 css = """
     <style>
         /* User Chat Message */
@@ -52,5 +18,4 @@ css = """
             width: 380px !important;
         }
     </style>
->>>>>>> 98bb46a9941ae260602baf3ff81947abe15e7280
     """
